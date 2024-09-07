ae90b1bd49980628d9fdea25164c520faa5c5edc
Katie-H258 <katie.hardy258@gmail.com>
2024-09-06 23:02:41 -0400

    finishes homework 0

--- Homework Project/Assets/Scenes/Scripts/HelloWorld.cs
+++ Homework Project/Assets/Scenes/Scripts/HelloWorld.cs
@@ -7,7 +7,15 @@ public class HelloWorld : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello, world!");   
+        Debug.Log("Hello, world!");
+        Debug.LogWarning("Warning: we're learning!");
+        Debug.LogError("Bames Nond's having a stronk, call the Bondulance");
+        // Author Katie Hardy
+        /* 
+           Are we allowed to use blender to make our own objects for use in the class? 
+           I'd love to make a game about an old town with a bunch of spooky citizens from folklore and they're friendly 
+           I'd love to learn about how people figure out how to tie together all the threads of games. Like easter eggs and fun character interactions. And sound design. 
+         */
     }
 
     // Update is called once per frame
